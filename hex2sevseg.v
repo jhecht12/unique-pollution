55ed2e4f681f6ae35c6997997cfb1c2454a1fcf9
jhecht12 <143758950+jhecht12@users.noreply.github.com>
2023-11-28 18:03:09 -0500

    Update hex2sevseg.v

--- hex2sevseg.v
+++ hex2sevseg.v
@@ -1,24 +1,7 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/12/2023 08:01:45 PM
-// Design Name: 
-// Module Name: hex2sevseg
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+//Converts a hexidecimal input to the seven segment cathodes necessary
+//0 bits in output indicate which cathodes are illuminated
 
 module hex2sevseg(
 input wire [3:0] x,
