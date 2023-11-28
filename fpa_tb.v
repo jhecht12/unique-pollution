7487825ce711a3d76354de53722f6ec2f3e3db09
jhecht12 <143758950+jhecht12@users.noreply.github.com>
2023-11-28 18:10:58 -0500

    Update fpa_tb.v

--- fpa_tb.v
+++ fpa_tb.v
@@ -1,24 +1,6 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/07/2023 11:08:58 PM
-// Design Name: 
-// Module Name: lab12_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+// Simulation source to test outputs of floating point adder
 
 module lab12_tb();
     reg clk = 0, clr = 0, go = 0;
