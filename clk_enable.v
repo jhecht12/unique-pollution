4ffa9ad83de1b756d90fbaa41fdec3687e72f1b8
jhecht12 <143758950+jhecht12@users.noreply.github.com>
2023-11-28 18:05:42 -0500

    Update clk_enable.v

--- clk_enable.v
+++ clk_enable.v
@@ -1,24 +1,6 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/26/2023 04:28:45 PM
-// Design Name: 
-// Module Name: clk_enable
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+//Clock enable is used to send out pulses less frequently, after a certain number of clock pulses
 
 module clk_enable(
 input clk,
